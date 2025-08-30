b7ffd505643b0ace5a5c9a83bec4988c5b2eeac7
alina-yur <alina.yurenko@gmail.com>
2025-08-30 12:50:00 +0000

    finalize the load

--- bench-native.sh
+++ bench-native.sh
@@ -13,11 +13,8 @@ psrecord $PID --plot "$(date +%s)-native.png" --max-cpu 3000 --max-memory 1000 -
 sleep 5
 print "Done waiting for the app to come up..."
 
-print "Starting the first iteration 1️⃣"
-hey -n=100000 http://localhost:8080/translations
-
-print "Starting the second iteration 2️⃣"
-hey -n=100000 http://localhost:8080/translations
+print "Starting the benchmark"
+hey -n=500000 http://localhost:8080/translations
 
 print "Done!"
 kill $PID
